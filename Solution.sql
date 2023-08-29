4ac3c2d23abfb8d672b2e760bfab9d50698ce71e
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-29 15:44:24 -0700

    faster implementation using IN keyword

--- Solution.sql
+++ Solution.sql
@@ -1,2 +1,5 @@
 --table Customers : id - int, name - varchar
 --table Orders : id - int, customerId - int
+
+SELECT name as "Customers" FROM Customers 
+WHERE Customers.id NOT IN (SELECT customerId FROM Orders);
